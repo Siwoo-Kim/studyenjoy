44188c6f7565e296bd928700c6b2ab58d76e236e
SiWoo Kim <35586824+Siwoo-Kim@users.noreply.github.com>
2018-09-07 18:04:50 -0700

    first commit

--- src/main/resources/static/app/scripts/controllers/main.js
+++ src/main/resources/static/app/scripts/controllers/main.js
@@ -11,24 +11,41 @@
 
   angular.module("frontendApp")
     .controller("MainCtrl", ["$scope", function ($scope) {
-      $scope.todos = [
-        {
-          title: "요가 수행",
-          completed: false,
-          createdAt: Date.now()
-        },
-        {
-          title: "앵귤러 학습",
-          completed: false,
-          createdAt: Date.now()
-        },
-        {
-          title: "운동하기",
-          completed: true,
-          createdAt: Date.now()
-        }
-      ];
+      $scope.remove = remove;
+      $scope.todos = todos();
 
+
+
+
+      function remove(todo /*$index*/) {
+        // $scope.todos.splice($index, 1);
+        let index = $scope.todos.findIndex(_todo => _todo.title == todo.title);
+        if(index > -1) {
+          $scope.todos.splice(index, 1);
+        }
+      }
+
+      function todos() {
+        return [
+          {
+            title: "요가 수행",
+            completed: false,
+            createdAt: Date.now()
+          },
+          {
+            title: "앵귤러 학습",
+            completed: false,
+            createdAt: Date.now()
+          },
+          {
+            title: "운동하기",
+            completed: true,
+            createdAt: Date.now()
+          }
+        ];
+      }
 
     }])
+
 })();
+
